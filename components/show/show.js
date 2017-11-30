de7d4ec93f07e5e492a9733b470cf3a73d333731
aaduru <ujwala_aaduru@yahoo.com>
2017-11-30 12:13:36 -0800

    Added button to show page

--- components/show/show.js
+++ components/show/show.js
@@ -30,13 +30,60 @@ class Show extends Component{
     debugger;
   }
 
+
+    navigate() {
+      this.props.navigator.push({
+        name: "root"
+      });
+    }
+
   render(){
+    var TouchableElement = TouchableHighlight;
+    if (Platform.OS === 'android') {
+    TouchableElement = TouchableNativeFeedback;
+    }
     return(
       <View>
         <Text> show </Text>
+        <Text style={styles.lable}> Check out restaurants </Text>
+
+        <TouchableElement
+          style={styles.button}
+          onPress={this.navigate.bind(this)}>
+          <Text style={styles.buttonText}> Back </Text>
+        </TouchableElement>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+
+
+button: {
+  flex: 1,
+  backgroundColor: 'steelblue',
+  width: 300,
+  borderWidth: 3,
+  borderColor: 'black',
+  borderRadius: 7,
+  alignItems: 'center',
+  justifyContent: 'center',
+},
+buttonText:{
+  fontFamily: 'Futura-CondensedExtraBold',
+  fontSize: 24,
+  textShadowOffset: {width: 1, height: 1},
+  textShadowColor: "blue",
+},
+lable:{
+  fontFamily: 'Futura-CondensedExtraBold',
+  fontSize: 24,
+  textShadowOffset: {width: 1, height: 1},
+  textShadowColor: "blue",
+}
+});
+
 
 export default Show;
