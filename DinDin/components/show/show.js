e2ba05b1994b374e584e53bae8dfe34c1598d978
Craig Scott <craigtscottjr@gmail.com>
2017-12-12 10:58:21 -0800

    DInDin: set stare with get request in show page

--- DinDin/components/show/show.js
+++ DinDin/components/show/show.js
@@ -22,13 +22,64 @@ class Show extends Component{
       position: this.props.navigator.state.routeStack[1].passProps.position,
       price: this.props.navigator.state.routeStack[1].passProps.price,
       radius: this.props.navigator.state.routeStack[1].passProps.radius,
-    };
+      resp: ["hi"],
 
+    };
+    // debugger;
   }
 
-  componentDidMount() {
+  componentWillMount() {
+    this._fetchYelpPost();
+    this._fetchYelpGet();
+    debugger;
+
+  }
+  componentHasMount() {
     debugger;
   }
+
+  _fetchYelpPost(){
+    var details = {
+      'client_id': '-qWaHcsd1FNhDOLm5IG1zw',
+      'client_secret': '2Z8KwxPhOiPr2NHmXzSGMppsocPykN4wLHaSl59icE0IRNNyEwHfCKO85ZZ071no',
+      'grant_type': 'client_credentials'
+    };
+
+    var formBody = [];
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    fetch('https://api.yelp.com/oauth2/token', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formBody
+    });
+  }
+
+  _fetchYelpGet() {
+      let data = {
+        method: 'GET',
+        headers: {
+          'Accept':       'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization':'Bearer y3Y6d_hVuQYBVSsBWZPhSJHsGx1uigpSZu5LGYv1Q3jTh6XMpOvaXG0O8NjNpFg5wJ3j2lE96pFTa8AXA7Mffg40PV6sOjbvE2R10Ie3kUz24Y_ONfCVpufsPH0gWnYx'
+        }
+      };
+      fetch('https://api.yelp.com/v3/businesses/search?term=restaurants&location=37.786882,-122.399972&limit=1', data)
+              .then(response => response.json())
+              .then((responceJson) => this.setState({resp: responceJson}))
+              .catch(error => {
+      console.error(error);
+    });;
+
+  };
 
 
     navigate() {
@@ -38,6 +89,7 @@ class Show extends Component{
     }
 
   render(){
+    console.log(this.state.resp);
     var TouchableElement = TouchableHighlight;
     if (Platform.OS === 'android') {
     TouchableElement = TouchableNativeFeedback;
@@ -46,7 +98,7 @@ class Show extends Component{
       <View style={styles.show}>
         <Text> show </Text>
         <Text style={styles.lable}> Check out restaurants </Text>
-
+        <Text> {this.state.resp[0]}</Text>
         <TouchableElement
           style={styles.button}
           onPress={this.navigate.bind(this)}>
